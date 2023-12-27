461e0d43aa0c2918d164820df67734b62b040a6a
Shubham <Shubhamshukla813@gmail.com>
2023-12-27 18:34:47 +0530

    new episode

--- App.js
+++ App.js
@@ -1,48 +1,61 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 import ReactDOM from "react-dom/client"; //earlier above import used to work but after upgrade have to add "react-dom/cliet"
-const heading1 = React.createElement(
-  "h1",
-  { id: "title", key: "h1" },
-  "Namaste React"
-); // it takes 3args (tag, attributes,props or {style:css}, "content or data lorem")
-const heading2 = React.createElement(
-  "h2",
-  { id: "title", key: "h2" },
-  "Namaste React H2"
-);
+// import "./index.css";
+// const heading1 = React.createElement(
+//   "h1",
+//   { id: "title", key: "h1" },
+//   "Namaste React"
+// ); // it takes 3args (tag, attributes,props or {style:css}, "content or data lorem")
+// const heading2 = React.createElement(
+//   "h2",
+//   { id: "title", key: "h2" },
+//   "Namaste React H2"
+// );
 // React element is an object
-// React.createElement gives an object : React.createElement() => Object (then converts itself into html-DOM) => html-DOM
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+// // React.createElement gives an object : React.createElement() => Object (then converts itself into html-DOM) => html-DOM
+// const container = React.createElement("div", { id: "container" }, [
+//   heading1,
+//   heading2,
+// ]);
 // ================JSX================
 //  JSX => React.createElement => object => html-DOM
-const heading = (
-  <h1 id="title" key="h1">
-    Namaste React JSX
-  </h1>
-);
+// const heading = (
+//   <h1 id="title" key="h1">
+//     Namaste React JSX
+//   </h1>
+// );
 // =======================React comp==================================
-const Title = () => <h1>Title react component</h1>;
+const Title = () => (
+  <div>
+    <h1>Foodie HUB</h1>
+  </div>
+);
 // ======================Functional Component ===============
-const HeaderComponent = () => {
+const App = () => {
   return (
     <div>
       <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
       {/* ===========OR========== */}
-      {Title()}
-      {heading}
-      <h2>Writing Component using JSX</h2>;
+      {/* {Title()} */}
+      {/* {heading} */}
+      {/* <h2>Writing Component using JSX</h2>; */}
     </div>
   );
 };
-const HeaderComponent2 = () => (
-  <div>
-    <h1>Writing Component using JSX</h1>;
-  </div>
-);
+// const HeaderComponent2 = () => (
+//   <div>
+//     <h1>Writing Component using JSX</h1>;
+//   </div>
+// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing a react element inside the root
 // root.render(container);
@@ -51,4 +64,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
 // ==== FOR component====
-root.render(<HeaderComponent />);
+root.render(<App />);
